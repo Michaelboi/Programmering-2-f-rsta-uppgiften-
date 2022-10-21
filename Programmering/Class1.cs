0d91f43bc27f92575be237df5b3875aaf8673e78
Michaelboi <micnwa050628@student.jenseneducation.se>
2022-10-21 19:58:12 +0200

    Add project files.

---
+++ Programmering/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Programmering2Exempel
+{
+    public class Bil
+    {
+        private int hjul = 4;
+        private string färg = "Blå";
+        private string motortyp = "Diesel";
+        
+
+        public int getHjul()
+        {
+            return this.hjul;
+        }
+        public string getFärg()
+        {
+            return this.färg;
+        }
+        public string getMotortyp()
+        {
+            return this.motortyp;
+        }
+        public void sethjul(int hjul)
+        {
+            this.hjul = hjul;
+
+        }
+        public void setFärg(string färg)
+        {
+            this.färg = färg;
+        }
+    }
+}
---
+++ Programmering/Program.cs
@@ -0,0 +1,44 @@
+﻿using Programmering2Exempel;
+using System;
+
+int beräknaArea(int sida1, int sida2)
+{
+    return sida1 * sida2;
+}
+Console.WriteLine(beräknaArea(5, 4));
+
+string Bilinfo = Console.ReadLine();
+
+
+Bil Bil1 = new Bil();
+Bil Bil2 = new Bil();
+Bil Bil3 = new Bil();
+
+
+Bil1.setFärg("Röd");
+Bil2.setFärg("Svart");
+Bil3.setFärg("Gul");
+Bil2.sethjul(6);
+Bil3.sethjul(7);
+
+// get/set
+
+Bil[] Array = new Bil[5];
+Array[0] = Bil1;
+Array[1] = Bil2;
+Array[2] = Bil3;
+
+// skriver man bil så får man fram information om en bil
+if (Bilinfo == "bil")
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.WriteLine("Bilen har " + Array[1].getHjul() + " hjul");
+        Console.WriteLine("Bilens färg är " + Array[1].getFärg());
+        break;
+    
+    }
+
+
+
+}
